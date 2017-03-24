35b954de8981b37d744d6d016f3f9d8d859aa112
mdatsev <martin.datsev.p@gmail.com>
2017-03-24 21:27:43 +0200

    added generation class and stuff

--- project/project/Creature.h
+++ project/project/Creature.h
@@ -14,5 +14,6 @@ public:
 	void AddMuscle(Muscle);
 	void CreateRandom(int min_nodes, int max_nodes, int min_size, int max_size, int min_friction, int max_friction, int min_restitution, int max_restitution, int min_mass, int max_mass, double max_dimension, int min_muscles, int max_muscles);
 	void CheckOverlap(Node& node, int overlapDistance);
+	Vec2 AverageDistance();
 };
 
